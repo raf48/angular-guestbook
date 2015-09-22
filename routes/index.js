21cad45e8e15cf7b3a8a46099081e2e3559e0fd1
Rafal <rafal.jadevic@gmail.com>
2015-09-22 18:00:57 +0300

    added admin.jade rendering function

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ exports.index = function(req, res) {
   res.render('index');
 };
 
+exports.admin = function(req, res) {
+  res.render('admin');
+};
+
 exports.partials = function(req, res) {
   var name = req.params.name;
   res.render('partials/' + name);
