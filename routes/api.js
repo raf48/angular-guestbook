a7a837112e81bb8ca2754c2f3a3669dff87c67bc
Rafal <rafal.jadevic@gmail.com>
2015-09-22 18:00:18 +0300

    added deleteMessages method

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,24 @@
 var fs = require('fs'),
-    data;
+    data,
+    current_id;
 
 /* Get messages */
 data = JSON.parse(fs.readFileSync('./data/messages.json', 'utf8'));
 
+/* Find current message id */
+for (var i = 0; i < data.length; i++) {
+  if (current_id < data[i].id) {
+    current_id = data[i].id;
+  }
+}
+
 /* GET */
 exports.getData = function(req, res) {
+  for (var i = 0; i < data.length; i++) {
+    data[i].id = i;
+  }
+  current_id = i;
+
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 };
@@ -15,12 +28,13 @@ exports.postData = function(req, res) {
   var newMsg = {},
       date = new Date();
 
+  newMsg.id = ++current_id;
   newMsg.from = req.body.from;
   newMsg.text = req.body.text;
   newMsg.date = date.toISOString();
   data.push(newMsg);
-  
-  res.send(data);
+
+  res.json(true);
 /*
   Uncomment this line if you want posted messages to be saved
   to an external "messages.json" file
@@ -29,4 +43,16 @@ exports.postData = function(req, res) {
     if (err) throw err;
     res.send(data);
   });*/
+};
+
+/* DELETE */
+exports.deleteMessage = function(req, res) {
+  var id = req.params.id;
+
+  if (id >= 0 && id < data.length) {
+    data.splice(id, 1);
+    res.json(true);
+  } else {
+    res.json(false);
+  }
 };
