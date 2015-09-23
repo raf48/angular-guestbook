d8a24809ba70b43ee836286860ba397ff9ee8c0a
Rafal <rafal.jadevic@gmail.com>
2015-09-24 01:11:17 +0300

    refactored adminController

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -62,20 +62,6 @@ app.controller('showMessages', function($scope, messagesAPI) {
 
 app.controller('adminController', function($scope, $route, messagesAPI) {
 
-  var deleteMessage, editMessage;
-  
-  $scope.promptDelete = function(id) {
-    return deleteMessage === id;
-  };
-  
-  $scope.showDelete = function(id) {
-    deleteMessage = id;
-  };
-  
-  $scope.cancelDelete = function() {
-    deleteMessage = "";
-  };
-
   $scope.delete = function(id) {
     messagesAPI.deleteMessage(id)
       .then(function(data) {
@@ -83,16 +69,6 @@ app.controller('adminController', function($scope, $route, messagesAPI) {
       }, function(reason) {
         console.log('failed to delete: ', reason);
       });
-    // Clear previous message delete prompts
-    $scope.cancelDelete();
-  };
-
-  $scope.editMessage = function(id) {
-    return editMessage === id;
-  };
-
-  $scope.edit = function(id, text) {
-    editMessage = id;
   };
 
   $scope.saveEdit = function(id, text) {
@@ -103,11 +79,5 @@ app.controller('adminController', function($scope, $route, messagesAPI) {
       }, function(reason) {
         console.log('failed to save edit: ', reason);
       });
-    $scope.cancelEdit();
-  };
-  
-  $scope.cancelEdit = function() {
-    editMessage = "";
-    $route.reload();
   };
 });
